63463dbb969c7f6446edb590400a6d1cf9a2de50
Gianniii <gianni.lodetti@hotmail.com>
2022-09-04 11:39:30 +0200

    Updated README

--- Projet2/main/java/ch/epfl/cs107/play/game/actor/bike/GameWithLevels.java
+++
@@ -1,9 +0,0 @@
-package ch.epfl.cs107.play.game.actor.bike;
-
-interface GameWithLevels {
-	// gère ce qui se passe lorsque la transition au niveau suivant doit se faire :
-	void nextLevel();
-
-	// gère ce qui se passe lorsque l'on veut recommencer le niveau courant :
-	void resetLevel();
-}
